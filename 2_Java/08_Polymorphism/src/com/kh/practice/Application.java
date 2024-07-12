a536faa38c66c69d855af135dfd509e6658164aa
kh240528 <133611346+kh240528@users.noreply.github.com>
2024-07-12 16:29:06 +0900

    자바 - 람다식, 스트림, 도서 관리 프로그램 풀이 (배열)

--- 2_Java/08_Polymorphism/src/com/kh/practice/Application.java
+++ 2_Java/08_Polymorphism/src/com/kh/practice/Application.java
@@ -3,18 +3,19 @@ package com.kh.practice;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import com.kh.practice.controller.BookController;
 import com.kh.practice.model.Book;
 import com.kh.practice.model.Member;
 
 public class Application {
 	
 	Scanner sc = new Scanner(System.in);
-	Member member = new Member();
+	BookController bc = new BookController();
 	Book[] books = {new Book("밥을 지어요", true, 0), 
 				new Book("오늘은 아무래도 덮밥", false, 0), 
 				new Book("원피스 108", false, 15), 
 				new Book("귀멸의 칼날 23", false, 19)};
-
+	
 	public static void main(String[] args) {
 		Application app = new Application();
 		app.menu();
@@ -23,40 +24,39 @@ public class Application {
 	public void menu() {
 		System.out.print("이름 : ");
 		String name = sc.nextLine();
-		member.setName(name);
+		
 		System.out.print("나이 : ");
 		int age = Integer.parseInt(sc.nextLine());
-		member.setAge(age);
 		
-		while(true) {
+		bc.addMember(name, age);
+		
+		boolean close = true;
+		while(close) {
 			System.out.println("==== 메뉴 ====");
 			System.out.println("1. 마이페이지");
 			System.out.println("2. 도서 대여하기");
 			System.out.println("3. 프로그램 종료하기");
 			System.out.print("메뉴 번호 : ");
-			int number = Integer.parseInt(sc.nextLine());
 			
-			if(number == 1) {
-				System.out.println(member);
-			} else if(number == 2) {
+			switch(Integer.parseInt(sc.nextLine())) {
+			case 1:
+				System.out.println(bc.myPage());
+				break;
+			case 2:
 				for(int i = 0; i < books.length; i++) {
 					System.out.println((i+1) + "번 도서 : " + books[i]);
 				}
 				System.out.print("대여할 도서 번호 선택 : ");
 				int select = Integer.parseInt(sc.nextLine());
-				// select(index+1) 번호에 따라서 해당 책이 Member - bookList에 추가
-				member.getBookList()[0] = books[select-1];
-				
-				
-				
+				System.out.println(bc.rentBook(books[select-1]));
+				break;
+			case 3:
+				close = false;
+				break;
 			}
-			
+	
 		}
 		
-		
-		
 	}
-	
-	
 
 }
