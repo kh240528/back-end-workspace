2fb9470fcf4d4980a2ad8828f66e03b6f04fd6c4
kh240528 <133611346+kh240528@users.noreply.github.com>
2024-07-19 17:21:52 +0900

    JDBC - BookMVC 풀이 중

--- 3_JDBC/05_BookMVC/src/com/kh/Application.java
+++ 3_JDBC/05_BookMVC/src/com/kh/Application.java
@@ -2,7 +2,10 @@ package com.kh;
 
 import java.util.Scanner;
 
-import com.kh.model.Member;
+import com.kh.controller.BookController;
+import com.kh.controller.MemberController;
+import com.kh.model.vo.Member;
+import com.kh.model.vo.Book;
 
 // 스키마 : sample
 // 테이블 : member, book, publisher, rent
@@ -13,6 +16,9 @@ public class Application {
 	
 	// 로그인 했을 시 사용자 정보를 담는 객체!
 	private Member member = new Member();
+	
+	private BookController bc = new BookController();
+	private MemberController mc = new MemberController();
 
 	public static void main(String[] args) {
 
@@ -60,36 +66,93 @@ public class Application {
 	// 1. 전체 책 조회
 	public void printBookAll() {
 		// 반복문을 이용해서 책 리스트 출력
+		for(Book book : bc.printBookAll()) {
+			String pubName = book.getPublisher().getPubName();
+			System.out.println("책 번호 : " + book.getBkNo()
+							+ " / 제목 : " + book.getBkTitle()
+							+ " / 저자 : " + book.getBkAuthor()
+							+ (pubName != null ? " / 출판사 : " + book.getPublisher().getPubName() : ""));
+		}
 	}
 
 	// 2. 책 등록
 	public void registerBook() {
 		// 책 제목, 책 저자를 사용자한테 입력 받아
-		// 기존 제목, 저자 있으면 등록 안되게!
-		// 등록에 성공하면 "성공적으로 책을 등록했습니다." 출력
-		// 실패하면 "책을 등록하는데 실패했습니다." 출력
+		System.out.print("책 제목 : ");
+		String title = sc.nextLine();
+		System.out.print("책 저자 : ");
+		String author = sc.nextLine();
+		
+		if(bc.registerBook(title, author)) {
+			// 등록에 성공하면 "성공적으로 책을 등록했습니다." 출력
+			System.out.println("성공적으로 책을 등록했습니다.");
+		} else {
+			// 실패하면 "책을 등록하는데 실패했습니다." 출력
+			System.out.println("책을 등록하는데 실패했습니다.");
+		}
+
 	}
 
 	// 3. 책 삭제
 	public void sellBook() {
 		// printBookAll로 전체 책 조회를 한 후
+		printBookAll();
+		
 		// 삭제할 책 번호 선택을 사용자한테 입력 받아
-		// 삭제에 성공하면 "성공적으로 책을 삭제했습니다." 출력
-		// 실패하면 "책을 삭제하는데 실패했습니다." 출력
+		System.out.print("삭제할 책 번호 : ");
+		int no = Integer.parseInt(sc.nextLine());
+		
+		if(bc.sellBook(no)) {
+			// 삭제에 성공하면 "성공적으로 책을 삭제했습니다." 출력
+			System.out.println("성공적으로 책을 삭제했습니다.");
+		} else {
+			// 실패하면 "책을 삭제하는데 실패했습니다." 출력
+			System.out.println("책을 삭제하는데 실패했습니다.");
+		}
+		
+		
+		
 	}
 
 	// 4. 회원가입
 	public void registerMember() {
 		// 아이디, 비밀번호, 이름을 사용자한테 입력 받아
-		// 회원가입에 성공하면 "성공적으로 회원가입을 완료하였습니다." 출력
-		// 실패하면 "회원가입에 실패했습니다." 출력
+		System.out.print("아이디 : ");
+		String id = sc.nextLine();
+		System.out.print("비밀번호 : ");
+		String password = sc.nextLine();
+		System.out.print("이름 : ");
+		String name = sc.nextLine();
+		
+		if(mc.registerMember(id, password, name)) {
+			// 회원가입에 성공하면 "성공적으로 회원가입을 완료하였습니다." 출력
+			System.out.println("성공적으로 회원가입을 완료하였습니다.");
+		} else {
+			// 실패하면 "회원가입에 실패했습니다." 출력
+			System.out.println("회원가입에 실패했습니다.");
+		}
+		
+		
 	}
 
 	// 5. 로그인
 	public void login() {
 		// 아이디, 비밀번호를 사용자한테 입력 받아 
-		// 로그인에 성공하면 "~~님, 환영합니다!" 출력 후 memberMenu() 호출
-		// 로그인에 성공하면 "~~님, 환영합니다!" 출력 후
+		System.out.print("아이디 : ");
+		String id = sc.nextLine();
+		System.out.print("비밀번호 : ");
+		String password = sc.nextLine();
+		member = mc.login(id, password);
+		if(member!=null) {
+			// 로그인에 성공하면 "~~님, 환영합니다!" 출력 후 
+			System.out.println(member.getMemberName() + "님, 환영합니다!");
+			// memberMenu() 호출
+			memberMenu();
+		} else {
+			// 실패하면 "로그인에 실패했습니다." 출력
+			System.out.println("로그인에 실패했습니다.");
+		}
+		
 	}
 
 	public void memberMenu() {
@@ -124,10 +187,14 @@ public class Application {
 
 	// 1. 책 대여
 	public void rentBook() {
+		// 같은 책을 여러 사용자가 대여할 수 있는지? 책이 1권이라고 가정!
+		// 다른 사람은 대여 못하게! 본인 뿐만 아니라 다른 사람도 대여 못하게!
+		// 기존 정보 삭제 후에 진행할게요!
 		// printBookAll 메서드 호출하여 전체 책 조회 출력 후
 		// 대여할 책 번호 선택을 사용자한테 입력 받아
+		// 이미 대여된 책은 대여 불가!
 		// 대여에 성공하면 "성공적으로 책을 대여했습니다." 출력
-		// 대여에 성공하면 "성공적으로 책을 대여했습니다." 출력
+		
 	}
 
 	// 2. 내가 대여한 책 조회
@@ -146,6 +213,8 @@ public class Application {
 
 	// 4. 회원탈퇴
 	public void deleteMember() {
+		// 회원탈퇴할 때 대여중인 책 있으면 탈퇴 못하게 막을까요?
+		// 아님 회원탈퇴시 대여중인 책들 모두 기록 삭제할까요?
 		// 회원탈퇴에 성공하면 "회원탈퇴 하였습니다 ㅠㅠ" 출력
 		// 실패하면 "회원탈퇴하는데 실패했습니다." 출력
 	}
---
+++ 3_JDBC/05_BookMVC/src/com/kh/model/vo/Member.java
@@ -0,0 +1,21 @@
+package com.kh.model.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor @Data
+public class Member {
+
+	private int memberNo;
+	private String memberId;
+	private String memberPwd;
+	private String memberName;
+	private char gender;
+	private String address;
+	private String phone;
+	private char status;
+	private Date enrollDate;
+}
