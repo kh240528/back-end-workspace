af9b2829c6c7cac3da0a4d45a0078588fdedb47c
kh240528 <133611346+kh240528@users.noreply.github.com>
2024-08-01 13:57:32 +0900

    마이바티스

--- 5_Spring/01_MyBatis/src/main/java/com/kh/mybatis/controller/MemberController.java
+++ 5_Spring/01_MyBatis/src/main/java/com/kh/mybatis/controller/MemberController.java
@@ -1,11 +1,15 @@
 package com.kh.mybatis.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kh.mybatis.model.dto.SearchDTO;
 import com.kh.mybatis.model.vo.Member;
 import com.kh.mybatis.service.MemberService;
 
@@ -67,6 +71,18 @@ public class MemberController {
 		if(vo.getName()==null) vo.setName(member.getName());
 		session.setAttribute("vo", vo);
 
+		return "redirect:/";
+	}
+	
+	@GetMapping("/search")
+	public String search(SearchDTO dto, Model model) {
+		model.addAttribute("search", service.search(dto));
+		return "index";
+	}
+	
+	@PostMapping("/delete")
+	public String delete(@RequestParam(name="idList", required=false) List<String> idList) {
+		if(idList!=null) service.delete(idList);
 		return "redirect:/";
 	}
 
