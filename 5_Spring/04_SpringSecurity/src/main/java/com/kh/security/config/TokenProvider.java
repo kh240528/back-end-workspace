9ccebca157b778ca2e9416bc8a2e96e4adbf96c4
kh240528 <133611346+kh240528@users.noreply.github.com>
2024-08-14 19:08:08 +0900

    Spring Security - Token 인증 방식

--- 5_Spring/04_SpringSecurity/src/main/java/com/kh/security/config/TokenProvider.java
+++ 5_Spring/04_SpringSecurity/src/main/java/com/kh/security/config/TokenProvider.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.kh.security.model.vo.Member;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -31,6 +32,19 @@ public class TokenProvider {
 				.setIssuedAt(new Date())
 				.setExpiration(Date.from(Instant.now().plus(1, ChronoUnit.DAYS)))
 				.compact();
+	}
+	
+	public Member validate(String token) {
+		Claims claims = Jwts
+							.parser()
+							.setSigningKey(secretKey)
+							.parseClaimsJws(token)
+							.getBody();
+		return Member.builder()
+				.id((String) claims.get("id"))
+				.name((String) claims.get("name"))
+				.role((String) claims.get("role"))
+				.build();
 	}
 
 }
